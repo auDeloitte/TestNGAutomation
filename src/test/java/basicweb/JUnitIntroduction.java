dfc4443b92315688932a07e6647d1004c325df3e
Ortiz <audortiz@deloitte.com>
2025-11-05 16:59:28 -0500

    updates

--- src/test/java/basicweb/JUnitIntroduction.java
+++ src/test/java/basicweb/JUnitIntroduction.java
@@ -1,28 +1,16 @@
 package basicweb;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.time.Duration;
-import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.ITestResult;
 
 import com.relevantcodes.extentreports.ExtentReports;
